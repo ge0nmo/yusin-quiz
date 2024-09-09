676609ae90f784018fd6253ae7238624b80697fe
ge0nmo <ge0nmo@naver.com>
2024-09-09 23:25:56 +0900

    [Test] subject rest docs

--- src/main/java/com/cpa/yusin/quiz/subject/controller/dto/request/SubjectUpdateRequest.java
+++ src/main/java/com/cpa/yusin/quiz/subject/controller/dto/request/SubjectUpdateRequest.java
@@ -1,9 +1,13 @@
 package com.cpa.yusin.quiz.subject.controller.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class SubjectUpdateRequest
 {
--- src/test/java/com/cpa/yusin/quiz/subject/controller/SubjectTest.java
+++ src/test/java/com/cpa/yusin/quiz/subject/controller/SubjectTest.java
@@ -6,6 +6,7 @@ import com.cpa.yusin.quiz.member.domain.type.Platform;
 import com.cpa.yusin.quiz.member.domain.type.Role;
 import com.cpa.yusin.quiz.member.service.port.MemberRepository;
 import com.cpa.yusin.quiz.subject.controller.dto.request.SubjectCreateRequest;
+import com.cpa.yusin.quiz.subject.controller.dto.request.SubjectUpdateRequest;
 import com.cpa.yusin.quiz.subject.domain.SubjectDomain;
 import com.cpa.yusin.quiz.subject.service.port.SubjectRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -139,27 +140,27 @@ public class SubjectTest
     void update() throws Exception
     {
         // given
-        subjectRepository.save(SubjectDomain.builder()
+        SubjectDomain subject = subjectRepository.save(SubjectDomain.builder()
                 .id(1L)
                 .name("회계학")
                 .build());
 
-        SubjectCreateRequest request = SubjectCreateRequest.builder()
-                .name("회계학")
+        SubjectUpdateRequest request = SubjectUpdateRequest.builder()
+                .name("경제학")
                 .build();
 
         String requestBody = objectMapper.writeValueAsString(request);
 
         // when
         ResultActions resultActions = mvc
-                .perform(post("/api/v1/admin/subject")
+                .perform(patch("/api/v1/admin/subject/" + subject.getId())
                         .content(requestBody)
                         .contentType(MediaType.APPLICATION_JSON));
 
         // then
         resultActions
-                .andExpect(status().isInternalServerError())
-                .andDo(document("과목 등록 - 중복된 이름",
+                .andExpect(status().isOk())
+                .andDo(document("과목 수정 - 성공",
                         preprocessRequest(prettyPrint()),
                         preprocessResponse(prettyPrint()),
                         requestFields(
@@ -167,8 +168,8 @@ public class SubjectTest
                         ),
 
                         responseFields(
-                                fieldWithPath("status").type(JsonFieldType.NUMBER).description("상태"),
-                                fieldWithPath("message").type(JsonFieldType.STRING).description("메세지")
+                                fieldWithPath("data.id").type(JsonFieldType.NUMBER).description("과목 고유 식별자"),
+                                fieldWithPath("data.name").type(JsonFieldType.STRING).description("과목 이름")
                         )
                 ));
     }
