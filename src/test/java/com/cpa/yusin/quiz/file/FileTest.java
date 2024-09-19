108e5cc40f9f61ee7eb5c5412c4d57aa386ec0cb
ge0nmo <ands0927@naver.com>
2024-09-19 17:15:43 +0900

    [Test] File test docs

--- src/test/java/com/cpa/yusin/quiz/file/FileTest.java
+++ src/test/java/com/cpa/yusin/quiz/file/FileTest.java
@@ -10,19 +10,22 @@ import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDoc
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.net.MalformedURLException;
 import java.net.URI;
-import java.net.URL;
-import java.nio.file.Paths;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.partWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParts;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -55,7 +58,17 @@ class FileTest
 
         // then
         resultActions
-                .andExpect(status().isCreated());
+                .andExpect(status().isCreated())
+                .andDo(document("파일 업로드",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        requestParts(
+                                partWithName("file").description("파일 업로드")
+                        ),
+                        responseFields(
+                                fieldWithPath("data.url").type(JsonFieldType.STRING).description("파일 URL")
+                        )
+                ));
     }
 
 }
