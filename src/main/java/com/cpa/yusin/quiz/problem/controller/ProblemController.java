095e4c8b5e435fe1392b05d4645400e8e6b5c927
ge0nmo <ands0927@naver.com>
2024-10-28 11:29:11 +0900

    [refactor] Problem Controller separation

--- src/main/java/com/cpa/yusin/quiz/problem/controller/ProblemController.java
+++ src/main/java/com/cpa/yusin/quiz/problem/controller/ProblemController.java
@@ -1,10 +1,9 @@
 package com.cpa.yusin.quiz.problem.controller;
 
 import com.cpa.yusin.quiz.common.controller.dto.response.GlobalResponse;
-import com.cpa.yusin.quiz.problem.controller.dto.request.ProblemRequest;
+import com.cpa.yusin.quiz.problem.controller.dto.response.ProblemDTO;
 import com.cpa.yusin.quiz.problem.controller.dto.response.ProblemResponse;
 import com.cpa.yusin.quiz.problem.controller.port.ProblemService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,20 +11,26 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/admin/problem")
+@RequestMapping("/api/v1/problem")
 @RestController
-public class AdminProblemController
+public class ProblemController
 {
     private final ProblemService problemService;
 
-    @PostMapping
-    public ResponseEntity<GlobalResponse<List<ProblemResponse>>> saveOrUpdate(@RequestParam(value = "examId") long examId,
-                                                                              @Valid @RequestBody List<ProblemRequest> requests)
+    @GetMapping("/{id}")
+    public ResponseEntity<GlobalResponse<ProblemDTO>> getById(@PathVariable("id") long id)
     {
-        problemService.saveOrUpdateProblem(examId, requests);
+        ProblemDTO response = problemService.getById(id);
+
+        return ResponseEntity.ok(new GlobalResponse<>(response));
+    }
+
+    @GetMapping
+    public ResponseEntity<GlobalResponse<List<ProblemResponse>>> getAllByExamId(@RequestParam("examId") long examId)
+    {
         List<ProblemResponse> response = problemService.getAllByExamId(examId);
 
-        return ResponseEntity
-                .ok(new GlobalResponse<>(response));
+        return ResponseEntity.ok(new GlobalResponse<>(response));
     }
+
 }
