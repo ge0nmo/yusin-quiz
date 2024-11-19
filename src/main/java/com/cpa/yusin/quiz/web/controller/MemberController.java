a5c1ce6ba67d1ef23feede3dc5055d5a5d79ba44
ge0nmo <ands0927@naver.com>
2024-11-19 18:10:36 +0900

    [Feat] Member page

--- src/main/java/com/cpa/yusin/quiz/web/controller/MemberController.java
+++ src/main/java/com/cpa/yusin/quiz/web/controller/MemberController.java
@@ -10,6 +10,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -20,12 +27,22 @@ public class MemberController
     private final MemberService memberService;
 
     @GetMapping("/member")
-    public String member(Model model)
+    public String member(Model model, @RequestParam("page")Optional<Integer> page, @RequestParam("size") Optional<Integer> size)
     {
-        Page<Member> response = memberService.getAllAdminNot(null, PageRequest.of(0, 1));
-        log.info("조회 완료");
+        int currentPage = page.orElse(1);
+        int pageSize = size.orElse(10);
+
+        Page<Member> response = memberService.getAllAdminNot(null, PageRequest.of(currentPage - 1, pageSize));
 
         model.addAttribute("members", response);
+
+        int totalPages = response.getTotalPages();
+        if(totalPages > 0){
+            List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages)
+                    .boxed()
+                    .toList();
+            model.addAttribute("pageNumbers", pageNumbers);
+        }
 
         return "member";
     }
