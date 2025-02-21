f2030246c96c45aab02c25821093e0fb6dd86940
ge0nmo <ands0927@naver.com>
2025-02-21 19:19:15 +0900

    [Refactor] Question Validation

--- src/main/java/com/cpa/yusin/quiz/question/controller/dto/request/QuestionRegisterRequest.java
+++ src/main/java/com/cpa/yusin/quiz/question/controller/dto/request/QuestionRegisterRequest.java
@@ -1,5 +1,7 @@
 package com.cpa.yusin.quiz.question.controller.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,11 +13,15 @@ import lombok.NoArgsConstructor;
 @Data
 public class QuestionRegisterRequest
 {
+    @NotBlank(message = "이름을 입력해주세요")
     private String username;
 
+    @NotBlank(message = "비밀번호를 입력해주세요")
     private String password;
 
+    @NotBlank(message = "제목을 입력해주세요")
     private String title;
 
+    @NotBlank(message = "내용을 입력해주세요")
     private String content;
 }
--- src/main/java/com/cpa/yusin/quiz/question/controller/dto/request/QuestionUpdateRequest.java
+++ src/main/java/com/cpa/yusin/quiz/question/controller/dto/request/QuestionUpdateRequest.java
@@ -1,5 +1,6 @@
 package com.cpa.yusin.quiz.question.controller.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,6 +12,9 @@ import lombok.NoArgsConstructor;
 @Data
 public class QuestionUpdateRequest
 {
+    @NotBlank(message = "제목을 입력해주세요")
     private String title;
+
+    @NotBlank(message = "내용을 입력해주세요")
     private String content;
 }
