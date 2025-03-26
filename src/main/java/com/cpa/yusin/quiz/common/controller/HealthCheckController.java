955c6f3cfcf9af26f6f434e844f59ff7b448ea22
ge0nmo <ands0927@naver.com>
2025-03-26 15:40:34 +0900

    [Fix] Health Check Controller URL

--- src/main/java/com/cpa/yusin/quiz/common/controller/HealthCheckController.java
+++ src/main/java/com/cpa/yusin/quiz/common/controller/HealthCheckController.java
@@ -3,11 +3,13 @@ package com.cpa.yusin.quiz.common.controller;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 import java.util.TreeMap;
 
+@RequestMapping("/api/v1")
 @RestController
 public class HealthCheckController
 {
